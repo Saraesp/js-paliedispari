4d9e577f412ae9b357dcfcde0f9d260159f28c12
Saraesp <saraesposito145@gmail.com>
2022-12-06 14:33:32 +0100

    commento problema pari e dispari es.

---
+++ Pari_Dispari.es2/js/script.js
@@ -0,0 +1,8 @@
+//1 - L'utente inserisce pari o dispari
+//2 - Inserisce in numero random da 1 a 5
+//3 - Generiamo attraverso una funzione un numero a caso sempre da 1 a 5 per il computer
+//4 - Sommiamo i due numeri
+//5 - Controllare se la somma dei numeri è pari o dispari
+//6 - SE la somma è pari restituisco hai vinto (true)
+//7 - ALTRIMENTI  se la somma è dispari hai perso (false)
+

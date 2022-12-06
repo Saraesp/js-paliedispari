c34140cdc9b452741f2f56818eaf1596ac5a5c75
Saraesp <saraesposito145@gmail.com>
2022-12-06 14:42:08 +0100

    Pari e Dispari es.

--- Pari_Dispari.es2/js/script.js
+++ Pari_Dispari.es2/js/script.js
@@ -6,3 +6,30 @@
 //6 - SE la somma è pari restituisco hai vinto (true)
 //7 - ALTRIMENTI  se la somma è dispari hai perso (false)
 
+function numeroRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) +min;
+}
+
+function checkPariDisoari(somma){
+    if(somma % 2 == 0){
+        return "pari";
+    }
+    
+    return "dispari";
+}
+
+let pari_dispari = prompt("Inserisci pari o dispari");
+let num_user = parseInt(prompt("Inserisci un numero tra 1 e 5"))
+
+let num_computer = numeroRandom(1,5);
+
+let somma = num_user + num_computer;
+
+let check = checkPariDisoari(somma);
+
+if(pari_dispari == check){
+    alert('Hai vinto')
+}
+else{
+    alert('Hai perso')
+}
